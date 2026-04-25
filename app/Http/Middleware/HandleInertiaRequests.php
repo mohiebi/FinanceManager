aa43482062dd6b46ab6a33fb540ea51e846b79f2
Mohi <e.mohamadhosein@gmail.com>
2026-04-25 13:54:54 +0330

    updating inertia request handler

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -41,6 +41,7 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
+            'authFlow' => fn () => $request->session()->get('auth_flow'),
             'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
         ];
     }

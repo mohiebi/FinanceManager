b63615f6d7d35393a96a0d9a937b130fe8e66c99
Mohi <e.mohamadhosein@gmail.com>
2026-04-26 13:44:54 +0330

    adding new routes for google auth

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\Auth\EmailAuthPageController;
 use App\Http\Controllers\Auth\WebEmailAuthController;
+use App\Http\Controllers\Auth\WebGoogleAuthController;
 use App\Http\Controllers\TransactionController;
+use App\Http\Middleware\EnsureProfileIsComplete;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Welcome')->name('home');
@@ -33,9 +35,15 @@ Route::middleware('guest')->group(function () {
     Route::post('auth/signup/complete', [WebEmailAuthController::class, 'completeSignup'])
         ->middleware('throttle:10,1')
         ->name('auth.signup.complete');
+
+    Route::get('auth/google/redirect', [WebGoogleAuthController::class, 'redirect'])
+        ->name('auth.google.redirect');
+
+    Route::get('auth/google/callback', [WebGoogleAuthController::class, 'callback'])
+        ->name('auth.google.callback');
 });
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'verified', EnsureProfileIsComplete::class])->group(function () {
     Route::get('dashboard', [TransactionController::class, 'index'])->name('dashboard');
     Route::resource('transactions', TransactionController::class)->only(['store', 'update', 'destroy']);
 });

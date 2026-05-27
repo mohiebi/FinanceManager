79192be72408f3f6214c285a9468a3fa6094c0bf
Mohi <e.mohamadhosein@gmail.com>
2026-05-27 20:34:42 +0330

    fixing sidebar width

--- resources/js/components/ui/sidebar/utils.ts
+++ resources/js/components/ui/sidebar/utils.ts
@@ -13,9 +13,9 @@ export type SidebarContext = {
 
 export const SIDEBAR_COOKIE_NAME = "sidebar_state"
 export const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
-export const SIDEBAR_WIDTH = "16rem"
-export const SIDEBAR_WIDTH_MOBILE = "18rem"
-export const SIDEBAR_WIDTH_ICON = "3rem"
+export const SIDEBAR_WIDTH = "13rem"
+export const SIDEBAR_WIDTH_MOBILE = "13rem"
+export const SIDEBAR_WIDTH_ICON = "5.75rem"
 export const SIDEBAR_KEYBOARD_SHORTCUT = "b"
 
 export const [useSidebar, provideSidebarContext] = createContext<SidebarContext>("Sidebar")

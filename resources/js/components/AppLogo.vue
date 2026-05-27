07de8508bb4f07ee1314ff77e8f77612114be39f
Mohi <e.mohamadhosein@gmail.com>
2026-05-27 20:34:00 +0330

    fixing logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
-import AppLogoIcon from '@/components/AppLogoIcon.vue';
+import logoGreen from '../../img/Logo-green.svg';
 </script>
 
 <template>
-    <div
-        class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground"
-    >
-        <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
-    </div>
+    <img :src="logoGreen" alt="CashPilot" class="h-8 w-7 shrink-0" />
     <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="mb-0.5 truncate leading-tight font-semibold"
-            >Laravel Starter Kit</span
+            >CashPilot</span
         >
     </div>
 </template>

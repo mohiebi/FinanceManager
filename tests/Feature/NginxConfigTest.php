f195d1430fc07dc3a4fd8e18863e377c04ade1c6
Mohi <e.mohamadhosein@gmail.com>
2026-07-02 15:54:56 +0330

    fixing nginx config

--- tests/Feature/NginxConfigTest.php
+++ tests/Feature/NginxConfigTest.php
@@ -1,10 +1,16 @@
 <?php
 
-test('docker nginx config allows large fastcgi response headers', function () {
-    $config = file_get_contents(base_path('docker/nginx.conf'));
+test('nginx configs allow large fastcgi response headers', function (string $path) {
+    $config = file_get_contents(base_path($path));
 
-    expect($config)->toContain('fastcgi_buffer_size     128k')
-        ->and($config)->toContain('fastcgi_buffers         4 256k')
-        ->and($config)->toContain('fastcgi_busy_buffers_size 256k')
-        ->and($config)->toContain('fastcgi_temp_file_write_size 256k');
-});
+    expect($config)->toContain('fastcgi_buffer_size')
+        ->and($config)->toContain('512k')
+        ->and($config)->toContain('fastcgi_buffers')
+        ->and($config)->toContain('16 512k')
+        ->and($config)->toContain('fastcgi_busy_buffers_size')
+        ->and($config)->toContain('1024k')
+        ->and($config)->toContain('fastcgi_temp_file_write_size');
+})->with([
+    'docker nginx config' => 'docker/nginx.conf',
+    'startup nginx config' => 'default',
+]);

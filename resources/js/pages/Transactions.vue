934a917f920196a0e1053754d5c2657c9c0af521
Mohi <e.mohamadhosein@gmail.com>
2026-07-10 14:31:56 +0330

    fixing opacity

--- resources/js/pages/Transactions.vue
+++ resources/js/pages/Transactions.vue
@@ -288,7 +288,7 @@
                                 </td>
                                 <td class="px-3 py-3.5 text-center sm:px-5">
                                     <div
-                                        class="flex items-center justify-center gap-1 opacity-0 transition-opacity group-hover:opacity-100"
+                                        class="flex items-center justify-center gap-1"
                                     >
                                         <button
                                             type="button"
@@ -462,7 +462,7 @@
                                 </td>
                                 <td class="px-3 py-3.5 text-center sm:px-5">
                                     <div
-                                        class="flex items-center justify-center gap-1 opacity-0 transition-opacity group-hover:opacity-100"
+                                        class="flex items-center justify-center gap-1"
                                     >
                                         <button
                                             type="button"

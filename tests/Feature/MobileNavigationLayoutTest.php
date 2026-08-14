18773793f533c6c29dcfb7b2701e6203f0b82972
Mohi <e.mohamadhosein@gmail.com>
2026-08-14 17:25:29 +0330

    fixing tests

--- tests/Feature/MobileNavigationLayoutTest.php
+++ tests/Feature/MobileNavigationLayoutTest.php
@@ -8,7 +8,9 @@ test('mobile navigation uses the sticky top header without a bottom nav', functi
 
     expect(resource_path('js/components/AppBottomNav.vue'))->not->toBeFile()
         ->and($layout)->not->toContain('AppBottomNav')
-        ->and($layout)->toContain('class="h-svh min-h-0 overflow-x-hidden overflow-y-auto pb-4 lg:pb-0"')
+        ->and($layout)->toContain('class="app-page-scroll h-svh')
+        ->and($layout)->toContain('overflow-y-auto pb-4')
+        ->and($layout)->not->toContain('lg:pb-0')
         ->and($shell)->toContain('class="h-svh min-h-svh flex-col overflow-hidden lg:flex-row"')
         ->and($header)->toContain('class="sticky top-0')
         ->and($header)->toContain('lg:static')

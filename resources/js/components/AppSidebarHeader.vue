e7be7bee525170e46dcf3d01a8c243d858505890
Mohi <e.mohamadhosein@gmail.com>
2026-08-19 14:27:11 +0330

    fixing static header for mobile

--- resources/js/components/AppSidebarHeader.vue
+++ resources/js/components/AppSidebarHeader.vue
@@ -156,7 +156,7 @@ function changeLocale(value: string) {
 
 <template>
     <header
-        class="sticky top-0 z-40 flex min-h-[64px] shrink-0 items-center border-b border-white/7 bg-[#111111]/88 px-4 py-3 text-white backdrop-blur-md transition-[width,height] ease-linear lg:min-h-[72px] lg:px-7 lg:py-4"
+        class="sticky top-0 z-40 flex min-h-[64px] shrink-0 items-center border-b border-white/7 bg-[#111111]/88 px-4 py-3 text-white backdrop-blur-md transition-[width,height] ease-linear lg:static lg:min-h-[72px] lg:px-7 lg:py-4"
     >
         <div
             class="flex w-full flex-col gap-2 lg:flex-row lg:items-center lg:justify-between lg:gap-4"
